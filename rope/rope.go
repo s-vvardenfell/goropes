0da9cd0e9a6a596f18e9eeda78af4718c6fe44c7
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-07-14 15:14:27 +0300

    find and display funcs in progress

--- rope/rope.go
+++ rope/rope.go
@@ -2,13 +2,15 @@ package rope
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
 
 type Rope struct {
 	Root     *RopeNode
-	raw      string //should store []rune to work with non-ascii
+	raw      string //should store runes to work with non-ascii
 	size     int
 	leafsize int
 }
@@ -38,6 +40,7 @@ func NewRopeFromString(raw string, leafsize int) (*Rope, error) {
 	r.raw = raw
 	r.leafsize = leafsize
 	r.Root = r.createRope(0, len(raw)-1)
+	r.Root = update(r.Root)
 	return &r, nil
 }
 
@@ -75,10 +78,74 @@ func (r *Rope) String() string {
 	return sb.String()
 }
 
-//TODO
-// func (r *Rope) update() {
+func update(root *RopeNode) *RopeNode {
+	if root != nil || root.Val != "" { // comparison with "" won't work with runes
+		return root
+	}
+	leftMost, rightMost := root.Right, root.Left
 
-// }
+	for leftMost != nil && leftMost.Val == "" {
+		leftMost = leftMost.Left
+	}
+
+	for rightMost != nil && rightMost.Val == "" {
+		rightMost = rightMost.Right
+	}
+
+	if rightMost != nil {
+		rightMost.Right = leftMost
+	}
+
+	if leftMost != nil {
+		leftMost.Left = rightMost
+	}
+	return root
+}
+
+// Utility for Find() func.
+// Returns a rope with single character at Root
+func (r *Rope) find(root *RopeNode, idx int) (*Rope, error) {
+	if idx < root.weight {
+		if root == nil && root.Val == "" {
+			return r.find(root.Left, idx)
+		}
+		return NewRopeFromString(string(root.Val[idx]), r.leafsize)
+	}
+	idx -= root.weight
+	return r.find(root.Right, idx)
+}
+
+// Returns value of rope at index 'i'
+// Returned value is also a Rope object
+func (r *Rope) Find(idx int) (*Rope, error) {
+	length := r.Size(nil)
+	if 0 <= idx && idx < length {
+		return r.find(r.Root, idx)
+	} else if -1*length <= idx && idx < 0 {
+		idx = length + idx
+	} else {
+		return nil, errors.New("index out of range")
+	}
+	return r.find(r.Root, idx)
+}
+
+//TODO should return the same val as RopeSize()? ('size') need tests
+func (r *Rope) Size(root *RopeNode) int {
+	if root == nil {
+		root = r.Root
+	}
+	head := root
+	count := 0
+
+	for head != nil {
+		count += head.weight
+		if head.Val != "" {
+			break
+		}
+		head = head.Right
+	}
+	return count
+}
 
 // //TODO
 // func (r *Rope) SetLeafSize(val int) error {
@@ -106,6 +173,69 @@ func (r *Rope) LeafSize() int {
 	return r.leafsize
 }
 
+//WRONG ANSWER RETURNS - need fix in ctor
 func (r *Rope) RopeSize() int {
 	return r.size
+}
+
+// Returns visual of Rope tree
+func (r *Rope) Display() {
+	lines, _, _, _ := r.displayAux(r.Root)
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+}
+
+// Returns list of strings, width, height and horizontal coordinate of the root
+func (r *Rope) displayAux(root *RopeNode) ([]string, int, int, int) {
+	// no child case
+	if root != nil && root.Val != "" {
+		line := visualizer(root)
+		width := len(line)
+		height := 1
+		middle := width / 2
+		return []string{line}, width, height, middle
+	}
+
+	left, n, p, x := r.displayAux(root.Left)
+	rigth, m, q, y := r.displayAux(root.Right)
+	s := visualizer(root)
+	u := len(s)
+
+	firstLine := strings.Repeat(" ", x+1) + strings.Repeat("_", n-x-1) + s + strings.Repeat("_", y) + strings.Repeat(" ", m-y)
+	secondLine := strings.Repeat(" ", x) + "/" + strings.Repeat(" ", n-x-1+u+y) + "\\" + strings.Repeat(" ", m-y-1)
+
+	if p < q {
+		for i := 0; i < q-p; i++ {
+			left = append(left, strings.Repeat(" ", n))
+		}
+	} else if q < p {
+		for i := 0; i < p-q; i++ {
+			rigth = append(rigth, strings.Repeat(" ", m))
+		}
+	}
+	// zippedLines := zip(left, rigth)
+	lines := []string{firstLine, secondLine}
+	return lines, n + m + u, max(p, q) + 2, n + u/2
+}
+
+func visualizer(root *RopeNode) string {
+	if root.Val != "" {
+		return root.Val
+	}
+	return strconv.Itoa(root.weight)
+}
+
+// func zip(left, right []string) string {
+// 	if root.Val != "" {
+// 		return root.Val
+// 	}
+// 	return strconv.Itoa(root.weight)
+// }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
