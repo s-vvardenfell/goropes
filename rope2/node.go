e74c4900c52e63b575c7d3d4dd6f91839c874b7d
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-07-15 14:00:16 +0300

    new rope with runes as underlying data

---
+++ rope2/node.go
@@ -0,0 +1,16 @@
+package rope2
+
+type Node struct {
+	Left   *Node // left child
+	Right  *Node // right child
+	Weight int   // left child size
+	// Height int   // for balancing
+	Data []rune
+}
+
+func NewNode(weight int, data []rune) *Node {
+	return &Node{
+		Weight: weight,
+		Data:   data,
+	}
+}
