95c6776d74f75386827ad2c813a2517a93e655a3
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-07-17 22:24:18 +0300

    upd gitignore

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/s-vvardenfell/goropes/rope"
+	rope "github.com/s-vvardenfell/goropes/rope"
 )
 
-const ( //not work with non-ascii yet
-	// stropeRu = "Строп (канат, корд) — структура данных, которая позволяет эффективно хранить и обрабатывать длинные строки, например текст."
+const (
+	stropeRu = "Строп (канат, корд) — структура данных, которая позволяет эффективно хранить и обрабатывать длинные строки, например текст."
 	stropeEn = "A sling (rope, cord) is a data structure that allows you to efficiently store and process long strings, such as text."
 )
 
 func main() {
-	r, err := rope.NewRopeFromString(stropeEn, 8)
+	r1, err := rope.NewGoRopeFromString(stropeRu, 8)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(r)
+	r1.Display()
+
+	r2, err := rope.NewGoRopeFromString(stropeEn, 8)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r2.Display()
+
+	r := r1.Concat(r2)
+
+	r.Display()
 }
